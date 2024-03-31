ac431bc3b18896d8df966b36bb297ea6ceb9b081
Eli Parra <eli@elzr.com>
2024-03-30 21:14:27 -0600

    paras properly trimmed!

--- backstage/scripts/format.rb
+++ backstage/scripts/format.rb
@@ -15,7 +15,10 @@ File.open(to_format, 'r') do |file|
 
         # escape %'s so that the textile span doesn't get confused
         inside_parens = inside_parens.gsub(/\%/, '&#37;')
-        if inside_parens =~ /^[A-Z\d,'′’–]+$/ # (A) or (A,C)
+
+        if prior_to_match =~ /<a[^>\/]+>[^<]*$/ # inside a link
+          next match
+        elsif inside_parens =~ /^[A-Z\d,'′’–]+$/ # (A) or (A,C)
           next match
         else
           extra = after_match =~ /\—|\]/ ? ' ' : ''
@@ -28,7 +31,7 @@ File.open(to_format, 'r') do |file|
       dash_dash = brackets.gsub(/—([^—]+)—/) do
         " <span class=\"paren\">#{$&}</span> "
       end
-      dash_end = dash_dash.gsub(/—([^—<]+)(\.|\?)/) do
+      dash_end = dash_dash.gsub(/—([^—<]+)(\.|\?|\.\")/) do
         " <span class=\"paren\">#{$&}</span> "
       end
       
--- js/code.js
+++ js/code.js
@@ -23,29 +23,60 @@ let R = {
     boot: ()=> {
       $('ul').each( function(i) {
         const ul = $(this);
+        const ul_id = ul.attr('id');
         const charCount = ul.text().length;
         const wordCount = ul.text().split(' ').length;
         const lineCount = ul.find('li').length;
 
         ul.data({char: charCount, word: wordCount, line: lineCount});
         ul.append(
-          `<label>${charCount} chars, ${wordCount} words, ${lineCount} lines</label>`);
-
-        let height = 60;
+          `<label id="${ul_id}">`+
+            `${ul_id}<br />`+
+            '<small>'+
+              `${charCount} chars, ${wordCount} words, ${lineCount} lines`+
+            '</small>'+
+          `</label>`);
         
-        if( charCount < 1500 ) {
-          height = 45;
-          if( charCount < 1000 ) {
-            height = 30;
-          }
-          console.log(charCount);
-        }
-
-        ul.css('height', `${height}rem`);
+        R.para.balancer(charCount, ul);
       });
     },
-    heighter: function() {
-      
+    balancer: function(charCount, ul) {
+      let height = 60;
+        
+      if( charCount < 1510 ) {
+        height = 45;
+        if( charCount < 1000 ) {
+          height = 30;
+        }
+      }
+
+      ul.css('height', `${height}rem`);
+      setTimeout(() => {
+        R.para.trimmer(ul);
+      }, 500);
+    },
+    trimmer: (ul)=>{
+      const ul_h = ul.innerHeight();
+      let li_h = [];
+      ul.find('li').each( function() {
+        li_h.push( $(this).outerHeight() );
+      });
+
+      const ul_id = ul.attr('id');
+      let li_sum = [0,0,0];
+      let li_col = 0;
+      let li_count = 0;
+      for (let i = 0; i < li_h.length; i++) {
+        // the +2 is there for fudging
+        if ( (li_sum[li_col] + li_h[i] + 2) > ul_h) {
+          li_col ++;
+        }
+        li_sum[li_col] += li_h[i];
+        li_count++;
+      }
+      const li_sum_max = Math.max(...li_sum);
+      console.log( {ul_id, ul_h, li_h, li_sum_max, li_sum, li_count} );
+      ul.height((li_sum_max + 5) + 'px');
     }
   },
   theme:{ // dark/light theme
